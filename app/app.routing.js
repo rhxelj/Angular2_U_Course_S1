de0651777d4473be68e0b02eb6f12d604aea7f14
Rogelio Martin Corsino <rcorsino@gmail.com>
2017-11-14 16:28:57 -0300

    Angular 2 Router

---
+++ app/app.routing.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var router_1 = require("@angular/router");
+//import the components you want to route
+var home_component_1 = require("./components/pages/home.component");
+var about_component_1 = require("./components/pages/about.component");
+var appRoutes = [
+    { path: '', component: home_component_1.HomeComponent },
+    { path: 'about', component: about_component_1.AboutComponent }
+];
+exports.routing = router_1.RouterModule.forRoot(appRoutes);
+//# sourceMappingURL=app.routing.js.map
